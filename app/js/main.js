6b08c2edf3c70daa604774bda4427d79be10d46a
Caesar <caesar.hinlo.tech@gmail.com>
2021-06-14 13:03:17 +0800

    [ER]Implementation for Burger mobile visibility set to 0 when burger is not activated and pressed

--- app/js/main.js
+++ app/js/main.js
@@ -2,17 +2,23 @@
 //// FOR HAMBURGER
 const menuBtn = document.querySelector('.menu-btn');
 const headerMobile = document.querySelector('.header__mobile');
+const headerMobileContent = document.querySelector('.header__mobile__navigation');
 headerMobile.classList.remove('header__mobile');
+headerMobileContent.style.display = "none";
+
+
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
     headerMobile.classList.add('header__mobile');
+    headerMobileContent.style.display = "block";
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
     headerMobile.classList.remove('header__mobile');
     menuOpen = false;
+    headerMobileContent.style.display = "none";
   }
 });
   
