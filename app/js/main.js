e87c466f6ea7a98cf0f74d51ece7fc7f6d92661c
Caesar <caesar.hinlo.tech@gmail.com>
2021-06-14 11:59:20 +0800

    [ER]Implementation for Burger mobile content bg to black and set z-indexes

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,17 @@
 
 //// FOR HAMBURGER
 const menuBtn = document.querySelector('.menu-btn');
+const headerMobile = document.querySelector('.header__mobile');
+headerMobile.classList.remove('header__mobile');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
+    headerMobile.classList.add('header__mobile');
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
+    headerMobile.classList.remove('header__mobile');
     menuOpen = false;
   }
 });
