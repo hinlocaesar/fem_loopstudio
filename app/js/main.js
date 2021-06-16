ad8b850184664c4cd73720279191a0e2e8900026
Caesar <caesar.hinlo.tech@gmail.com>
2021-06-16 11:26:26 +0800

    [DEFECT]Mobile 1st start up and refesh shows hamburger black sceen

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,14 @@
-
-//// FOR HAMBURGER
+/********************
+TITLE: IMPLEMENTATION FOR HAMBURGER
+AUTHOR: CAESAR HINLO
+REVISION DATE: 6/16/2021
+*********************/
 const menuBtn = document.querySelector('.menu-btn');
 const headerMobile = document.querySelector('.header__menu');
 const headerMobileContent = document.querySelector('.header__menu__navigation');
 headerMobile.classList.remove('header__menu');
 headerMobileContent.style.display = "none";
+headerMobile.style.visibility="hidden";
 
 
 let menuOpen = false;
@@ -14,6 +18,7 @@ menuBtn.addEventListener('click', () => {
     headerMobile.classList.add('header__menu');
     headerMobileContent.style.display = "block";
     menuOpen = true;
+    headerMobile.style.visibility="visible";
   } else {
     menuBtn.classList.remove('open');
     headerMobile.classList.remove('header__menu');
