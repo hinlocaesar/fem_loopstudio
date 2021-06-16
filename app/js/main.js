111c5fd1d6c3638a7d473bee754dd7a95752c299
Caesar <caesar.hinlo.tech@gmail.com>
2021-06-16 10:49:18 +0800

    [ER][MOBILE]Changed naming of mobile to menu to cater desktop behavior

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,9 @@
 
 //// FOR HAMBURGER
 const menuBtn = document.querySelector('.menu-btn');
-const headerMobile = document.querySelector('.header__mobile');
-const headerMobileContent = document.querySelector('.header__mobile__navigation');
-headerMobile.classList.remove('header__mobile');
+const headerMobile = document.querySelector('.header__menu');
+const headerMobileContent = document.querySelector('.header__menu__navigation');
+headerMobile.classList.remove('header__menu');
 headerMobileContent.style.display = "none";
 
 
@@ -11,12 +11,12 @@ let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
-    headerMobile.classList.add('header__mobile');
+    headerMobile.classList.add('header__menu');
     headerMobileContent.style.display = "block";
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
-    headerMobile.classList.remove('header__mobile');
+    headerMobile.classList.remove('header__menu');
     menuOpen = false;
     headerMobileContent.style.display = "none";
   }
